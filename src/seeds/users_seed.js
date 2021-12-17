db978335693fea72006a62d312338cbbbbe3450d
Rochim Farul Noviyan <rochim.noviyan@gmail.com>
2021-12-17 22:10:54 +0700

    Fix: seeding

--- src/seeds/users_seed.js
+++ src/seeds/users_seed.js
@@ -3,16 +3,24 @@ import dotenv from 'dotenv';
 
 const seed = function(knex) {
   dotenv.config();
+  const insert = async function() {
+    const password = await bcrypt.hash(
+        process.env.PASSWORD || 'password', 10);
+    return knex('users').insert([
+      {id: 1,
+        email: 'rochim.noviyan@gmail.com',
+        password,
+      },
+    ]);
+  };
+
   return knex('users').del()
-      .then(async function() {
-        const password = await bcrypt.hash(
-            process.env.PASSWORD || 'password', 10);
-        return knex('users').insert([
-          {id: 1,
-            email: 'rochim.noviyan@gmail.com',
-            password,
-          },
-        ]);
+      .then( (_) => {
+        insert();
+      })
+      .catch((err) => {
+        if (err) console.log(err.message);
+        insert();
       });
 };
 
