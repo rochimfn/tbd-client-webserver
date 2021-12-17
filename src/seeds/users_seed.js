03c82e9451dff1d6086a0761323b887686db616e
Rochim Farul Noviyan <rochim.noviyan@gmail.com>
2021-12-17 22:17:36 +0700

    Fix database setup

--- src/seeds/users_seed.js
+++ src/seeds/users_seed.js
@@ -3,24 +3,16 @@ import dotenv from 'dotenv';
 
 const seed = function(knex) {
   dotenv.config();
-  const insert = async function() {
-    const password = await bcrypt.hash(
-        process.env.PASSWORD || 'password', 10);
-    return knex('users').insert([
-      {id: 1,
-        email: 'rochim.noviyan@gmail.com',
-        password,
-      },
-    ]);
-  };
-
   return knex('users').del()
-      .then( (_) => {
-        insert();
-      })
-      .catch((err) => {
-        if (err) console.log(err.message);
-        insert();
+      .then(async function() {
+        const password = await bcrypt.hash(
+            process.env.PASSWORD || 'password', 10);
+        return knex('users').insert([
+          {id: 1,
+            email: 'rochim.noviyan@gmail.com',
+            password,
+          },
+        ]);
       });
 };
 
